08b361aa349cdc42169d0c7653f2ef186ff672a7
Mohammad Salehnia <12606608+mohammadsalehnia@users.noreply.github.com>
2023-12-08 15:06:22 +0330

    Move Product and Subscription models to UsersAccess dir

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Src\UsersAccess\Models\Subscription;
 
 class User extends Authenticatable
 {
